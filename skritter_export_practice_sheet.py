a921f777af300c29c4759bf9e3a3c1c0f1d19724
Attila M. Magyar <attila.m.magyar@gmail.com>
2015-05-05 19:13:54 +0200

    make skritter printed practice sheets more flexible

--- skritter_export_practice_sheet.py
+++ skritter_export_practice_sheet.py
@@ -12,6 +12,15 @@ HTML = """\
     table.cards th,
     table.cards td {
         border-collapse: collapse;
+    }
+
+    table.cards {
+        margin-top: -2px;
+        border: solid 2px black;
+    }
+
+    table.cards th,
+    table.cards td {
         border: solid 1px black;
         font-weight: normal;
     }
@@ -28,25 +37,20 @@ HTML = """\
     }
 
     table.cards th:nth-child(1) {
-        width: 4cm;
+        width: 3.5cm;
     }
 
     table.cards th:nth-child(2) {
-        width: 3cm;
+        width: 3.5cm;
     }
 
     table.cards,
-    table.cards tbody,
     table.cards td,
     table.cards th,
     table.cards th div {
         break-inside: avoid;
         page-break-inside: avoid;
         -webkit-column-break-inside: avoid;
-    }
-
-    table.cards tbody {
-        border: solid 2px black;
     }
 
     .brush,
@@ -61,25 +65,23 @@ HTML = """\
 
     .brush {
         font-family: nagayama_kai;
-        font-size: 0.35cm;
+        font-size: 0.45cm;
     }
 
     .print {
         font-family: serif;
-        font-size: 0.25cm;
+        font-size: 0.35cm;
     }
 
     .meaning {
         font-family: Verdana, sans-serif;
-        font-size: 0.25cm;
+        font-size: 0.3cm;
         text-align: right;
     }
     </style>
 </head>
 <body>
-    <table class="cards">
-%s
-    </table>
+    %s
 </body>
 </html>
 """
@@ -89,7 +91,6 @@ cells = "".join(["<td></td>" for i in range(1, 11)])
 
 with open("skritter.txt") as f:
     cards = []
-    count = 0
 
     for line in f:
         kanji, kana, meaning = line.strip().split("\t")
@@ -98,27 +99,31 @@ with open("skritter.txt") as f:
             cards.append((kanji, kana, meaning))
 
     for kanji, kana, meaning in cards:
-        count += 1
+        extra_rows = max(1, int((len(kanji.strip()) * 7) / 10))
         cards_html += '''\
-            <tbody>
+            <table class="cards">
                 <tr>
-                    <th rowspan="2">
+                    <th rowspan="{rowspan}">
                         <div>
                             <div><span class="brush">{kanji}</span></div>
                             <div><span class="print">{kanji}</span></div>
                             <div><span class="print">{kana}</span></div>
                         </div>
                     </th>
-                    <th rowspan="2">
+                    <th rowspan="{rowspan}">
                         <div class="meaning">{meaning}</div>
                     </th>
                     {cells}
                 </tr>
-                <tr>{cells}</tr>
-            </tbody>
-            '''.format(kanji=kanji, kana=kana, meaning=meaning, cells=cells)
-
-        if count % 10 == 0:
-            cards_html += "</table><table class=\"cards\">"
+                {rows}
+            </table>
+            '''.format(
+                kanji=kanji,
+                kana=kana,
+                meaning=meaning,
+                cells=cells,
+                rows="</tr><tr>".join(cells for i in range(0, extra_rows)),
+                rowspan=extra_rows + 1
+            )
 
 print(HTML % cards_html)
