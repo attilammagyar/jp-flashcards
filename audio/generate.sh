4d159d8383cb3fb2787f9b78a98122042e80ab0c
Attila M. Magyar <attila.m.magyar@gmail.com>
2016-03-15 17:59:43 +0100

    fix audio for #1333

--- audio/generate.sh
+++ audio/generate.sh
@@ -21,19 +21,31 @@ cat text.txt \
         gender="male"
         if [[ $(($i%2)) -eq 1 ]]
         then
-            gender=female
+            gender="female"
         fi
         if [[ ! -e "$f.wav" ]] && [[ ! -e "$f.mp3" ]]
         then
+            url_text=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' $text)
+            # curl \
+                # "http://api.microsofttranslator.com/v2/http.svc/speak?appId=$app_id&language=ja-JP&format=audio/wav&options=MaxQuality|$gender&text=$url_text" \
+                # -H 'Host: api.microsofttranslator.com' \
+                # -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0' \
+                # -H 'Accept: audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5' \
+                # -H 'Accept-Language: en-US,en;q=0.5' \
+                # -H 'DNT: 1' \
+                # -H 'Range: bytes=0-' \
+                # -H 'Referer: http://www.bing.com/translator/' \
+                # -H 'Connection: keep-alive' > "$f.wav"
+
             curl \
-                "http://api.microsofttranslator.com/v2/http.svc/speak?appId=$app_id&language=ja-JP&format=audio/wav&options=MaxQuality|$gender&text=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' $text)" \
-                -H 'Host: api.microsofttranslator.com' \
-                -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0' \
+                "http://www.bing.com/translator/api/language/Speak?locale=ja-JP&gender=$gender&media=audio/wav&text=$url_text" \
+                -H 'Host: www.bing.com' \
+                -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0' \
                 -H 'Accept: audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5' \
                 -H 'Accept-Language: en-US,en;q=0.5' \
-                -H 'DNT: 1' \
                 -H 'Range: bytes=0-' \
-                -H 'Referer: http://www.bing.com/translator/' \
+                -H 'Referer: http://www.bing.com/translator' \
+                -H 'Cookie: mtstkn=Sy38fSIayZK2cGoWGEGUn1mNmAqbUo5BMLmpWpshyLHtIT8OVbrdM6BRmspeE4%2B6; _EDGE_S=F=1&SID=0C49EF07471769060583E7DE46E568C6; _EDGE_V=1; MUID=1128BAC3D836616A3492B21AD9C46029; MUIDB=1128BAC3D836616A3492B21AD9C46029; srcLang=-; destLang=en; smru_list=; dmru_list=en; destDia=en-US; sourceDia=ja-JP; WLS=C=&N=; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=6E09F2A61B604B828E60ADC13690A0EE; SRCHUSR=DOB=20160315; _SS=SID=0C49EF07471769060583E7DE46E568C6' \
                 -H 'Connection: keep-alive' > "$f.wav"
         fi
         if [[ ! -e "$f.mp3" ]]
